cfc26779af587d088cad28a2c412eea3b6d00d6e
Nazmul Idris <nazmul@google.com>
2018-08-21 13:38:30 -0700

    Initial commit

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,32 @@
 <template>
-  <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+    <v-app>
+        <v-toolbar app>
+            <v-icon>{{app_icon}}</v-icon>
+            <v-toolbar-title v-text="title"></v-toolbar-title>
+            <v-spacer></v-spacer>
+        </v-toolbar>
+        <v-content>
+            <HelloWorld v-bind:msg="body_message"/>
+        </v-content>
+        <v-footer app>
+            <v-spacer></v-spacer>
+            <span>{{footer_message}}</span>
+            <v-spacer></v-spacer>
+        </v-footer>
+    </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+    import HelloWorld from './components/HelloWorld';
+    import {appData} from "./main";
 
-export default {
-  name: 'app',
-  components: {
-    HelloWorld
-  }
-}
+    export default {
+        name: 'App',
+        components: {
+            HelloWorld
+        },
+        data: () => {
+            return appData;
+        }
+    };
 </script>
-
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,16 @@
-import Vue from 'vue'
-import App from './App.vue'
+import '@babel/polyfill';
+import Vue from 'vue';
+import './plugins/vuetify';
+import App from './App.vue';
+import 'material-design-icons-iconfont/dist/material-design-icons.css';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
 
-new Vue({
-  render: h => h(App)
-}).$mount('#app')
+export const appData = {
+    app_icon: 'code',
+    body_message: 'Hello world!',
+    footer_message: 'Made with ❤️ in Mountain View, California',
+    title: 'Vue, Vuetify, Webpack'
+};
+
+new Vue({render: h => h(App)}).$mount('#app');
